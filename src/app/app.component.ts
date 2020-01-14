0c09d84ed77bec952a510c58af7758cfa0fa1281
Javier Iniesta <jiniesta@grupoarpada.com>
2020-01-14 12:05:49 +0100

    [IMP] Date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,5 @@ export class AppComponent {
   valorDePromesa = new Promise((resolve, reject) => {
     setTimeout(() => resolve('Llegó la data!'), 3500);
   });
+  fecha = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LOCALE_ID } from '@angular/core';
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'es'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
