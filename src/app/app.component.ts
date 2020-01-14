ba3a47797b5eca953eceb325dd8dd3039803fc5f
Javier Iniesta <jiniesta@grupoarpada.com>
2020-01-14 12:25:20 +0100

    [IMP] Capitalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre = 'Javier';
+  nombre2 = 'javIEr iniESta aseNSio';
   arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   PI = Math.PI;
   a = 0.234;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { registerLocaleData } from '@angular/common';
 registerLocaleData(localeEs);
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
